d09a27ec017f18566e9770ae64f6e3607a8e4495
Asif <ashid8bd@gmail.com>
2015-03-19 03:47:15 +0600

    buy skock feature added

--- public/modules/core/controllers/mystocks.client.controller.js
+++ public/modules/core/controllers/mystocks.client.controller.js
@@ -3,14 +3,25 @@
 
 angular.module('core').controller('MystocksController', ['$http', '$scope', 'Authentication',
     function($http, $scope, Authentication) {
-        
-        
+
+        $scope.buy = function() {
+            console.log($scope.myItem.sym);
+            console.log($scope.myItem.price);
+            console.log($scope.qty);
+            console.log($scope.qty*$scope.myItem.price);
+            $scope.myItem=undefined;
+             $scope.msg='1 item is added to your portfolio.';
+
+        };
+
         $http.get('/getmystocks').success(function(response) {
 
             $scope.stocks = response;
-           
-            
+
+
             $scope.qty = 0;
+            
+            $scope.amount = 77;
 
             //console.
 

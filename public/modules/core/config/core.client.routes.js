c1c65d40f8a78382062239d8d4d700807fb3dc09
mominwork <momin.work.bd@gmail.com>
2015-05-20 13:28:55 +0600

    sudoku link

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -2,31 +2,33 @@
 
 // Setting up route
 angular.module('core').config(['$stateProvider', '$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider) {
-		// Redirect to home view when route not found
-		$urlRouterProvider.otherwise('/');
+    function($stateProvider, $urlRouterProvider) {
+        // Redirect to home view when route not found
+        $urlRouterProvider.otherwise('/');
 
-		// Home state routing
-		$stateProvider.
-		state('home', {
-			url: '/',
-			templateUrl: 'modules/core/views/home.client.view.html'
-		}).state('tutorial', {
-			url: '/tutorial',
-			templateUrl: 'modules/core/views/tutorial.client.view.html'
-		}).
-		state('drag', {
-			url: '/drag',
-			templateUrl: 'modules/core/views/drag.client.view.html'
-		}).state('timer', {
-			url: '/timer',
-			templateUrl: 'modules/core/views/timer.client.view.html'
-		}).state('stockinfo', {
-			url: '/stockinfo',
-			templateUrl: 'modules/core/views/dse.client.view.html'
-		});
+        // Home state routing
+        $stateProvider.
+        state('home', {
+            url: '/',
+            templateUrl: 'modules/core/views/home.client.view.html'
+        }).state('tutorial', {
+            url: '/tutorial',
+            templateUrl: 'modules/core/views/tutorial.client.view.html'
+        }).state('drag', {
+            url: '/drag',
+            templateUrl: 'modules/core/views/drag.client.view.html'
+        }).state('timer', {
+            url: '/timer',
+            templateUrl: 'modules/core/views/timer.client.view.html'
+        }).state('stockinfo', {
+            url: '/stockinfo',
+            templateUrl: 'modules/core/views/dse.client.view.html'
+        }).state('sudoku', {
+            url: '/sudoku',
+            templateUrl: 'modules/core/views/sudoku.client.view.html'
+        });
 
 
 
-	}
+    }
 ]);
